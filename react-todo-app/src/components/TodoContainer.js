dd9d57e826032333b5e52e0b361aa0d35ee75df1
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-01 23:16:53 +0100

    add a new todo item

--- react-todo-app/src/components/TodoContainer.js
+++ react-todo-app/src/components/TodoContainer.js
@@ -50,7 +50,14 @@ class TodoContainer extends React.Component {
   };
 
   addTodoItem = title => {
-    console.log(title);
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
   };
 
    render() {
