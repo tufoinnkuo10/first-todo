d7eae60631cf091b2344f097c76b159d4c8d2c42
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-02 22:19:27 +0100

    adding the nav bar and other navigations

--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -1,9 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import TodoContainer from "./functionBased/components/TodoContainer"
-import { BrowserRouter } from "react-router-dom"
-
-//stylesheet
+import { BrowserRouter as Router } from "react-router-dom"
 import "./functionBased/App.css"
 
 ReactDOM.render(
