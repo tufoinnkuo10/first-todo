c4cd200880f3fa0476a888eaaad5cb754511599a
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-02 17:37:07 +0100

    add the Browser ro0uter and import it in the index.js

--- react-todo-app/src/index.js
+++ react-todo-app/src/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import TodoContainer from "./functionBased/components/TodoContainer"
+import { BrowserRouter } from "react-router-dom"
 
 //stylesheet
 import "./functionBased/App.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router>
+      <TodoContainer />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 )
